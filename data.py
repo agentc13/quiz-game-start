9cdb047d4bc2f4b1755c05c517e0c973a4fce815
agentc13 <1077509+agentc13@users.noreply.github.com>
2023-04-09 13:41:57 -0400

    Initial commit

---
+++ data.py
@@ -0,0 +1,103 @@
+question_data = [
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "medium",
+                "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "The logo for Snapchat is a Bell.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "medium",
+                "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "medium",
+                "question": "The open source program Redis is a relational database server.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "medium",
+                "question": "AMD created the first consumer 64-bit processor.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "hard",
+                "question": "DHCP stands for Dynamic Host Configuration Port.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "The Windows ME operating system was released in the year 2000.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "medium",
+                "question": "Android versions are named in alphabetical order.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "category": "Science: Computers",
+                "type": "boolean",
+                "difficulty": "easy",
+                "question": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            }
+        ]
+
